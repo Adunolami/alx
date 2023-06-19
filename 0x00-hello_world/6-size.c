e2e79150d2564c9c62627597265ad329e4a63579
Adunolami <abinatitilayo2@gmail.com>
2023-06-19 08:46:26 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
-#inclde <stdio.h>
+#include <stdio.h>
 /**
  * main - A program that prints the size of various types of comuter types
- * Return: 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -11,9 +11,9 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %zu byte(s)", (unsigned long)sizeof(d));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
