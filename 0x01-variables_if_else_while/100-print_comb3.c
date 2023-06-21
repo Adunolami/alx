4e0e7f135d2306bc8ac011bc41f1538ea7bd3ee8
Adunolami <abinatitilayo2@gmail.com>
2023-06-21 07:30:13 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *  Return: Always 0
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (n = 49; m <= 57; m++)
+		{
+			if (n > m)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
- * main - Prints all possible combinations of single-digit numbers
+ * main -prints all possible combination of single digit number
  * Return: Always 0
  */
 
 int main(void)
 {
-	int num;
+	int i = '0';
 
-for (num = 0; num <= 9; num++)
-
-	putchar((num %10) + '0');
-		if (num == 9)
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
 		{
-			putchar(', ');
+			putchar(',');
 			putchar(' ');
+		}
+		++i;
 	}
 	putchar('\n');
 	return (0);
