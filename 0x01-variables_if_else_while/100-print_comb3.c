e7226acbf85d0e81ce85fd0e346d7c61319a0294
Adunolami <abinatitilayo2@gmail.com>
2023-06-21 11:30:50 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,24 +9,22 @@
 
 int main(void)
 {
-	int n, m;
+	int d, p;
 
-	for (n = 48; n <= 56; n++)
+	for (d = '0'; d < '9'; d++)
 	{
-		for (n = 49; m <= 57; m++)
-		{
-			if (n > m)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-
-				}
-			}
-		}
+	for (p = d + 1; p <= 9; p++)
+	{
+	if (!(p - d))
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
