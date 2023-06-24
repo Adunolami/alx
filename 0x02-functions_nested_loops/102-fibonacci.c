f497fdbac9f4fa09edd832763ccb0a6633e6bf7b
Adunolami <abinatitilayo2@gmail.com>
2023-06-24 02:21:37 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,*        separated by a comma followed by a space.
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *     separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -27,7 +28,5 @@ int main(void)
 		else
 			printf(", ");
 	}
-
-
 	return (0);
 }
